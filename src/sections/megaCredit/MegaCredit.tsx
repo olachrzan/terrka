42004f03f69c355dbca7991d4dd68ae9a457caf0
Ola Chrzan <ola.chrzan16@gmail.com>
2024-03-09 16:15:47 +0100

    Improve styles

--- src/sections/megaCredit/MegaCredit.tsx
+++ src/sections/megaCredit/MegaCredit.tsx
@@ -8,7 +8,7 @@ export const MegaCredit = () => {
   return (
     <div className={styles.wrapper}>
       <img className={styles.icon} src={icon} alt={''} />
-      <ProductionField />
+      <ProductionField defaultValue={0} />
     </div>
   )
 };
