3d73696e8d9135b92ad9fed5e067cf6dac98919e
Ola Chrzan <ola.chrzan16@gmail.com>
2024-03-09 15:58:41 +0100

    Create MegaCredit component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import styles from './app.module.scss';
+import { MegaCredit } from './sections/megaCredit/MegaCredit';
 
 const App = () => (
-  <h1 className={styles.header}>TFM - resources manager</h1>
+  <>
+    <h1 className={styles.header}>TFM - resources manager</h1>
+    <MegaCredit />
+  </>
 );
 
 export default App;
---
+++ src/sections/megaCredit/MegaCredit.tsx
@@ -0,0 +1,14 @@
+import { ProductionField } from '../../components/productionField/ProductionField';
+import styles from './megaCredit.module.scss';
+import icon from './icon.png';
+
+export const MegaCredit = () => {
+
+
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.icon} src={icon} alt={''} />
+      <ProductionField />
+    </div>
+  )
+};
