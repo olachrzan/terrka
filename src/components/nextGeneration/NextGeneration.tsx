fd661634244bfb63fd05a91332be4ae3350d4475
Ola Chrzan <ola.chrzan16@gmail.com>
2024-06-22 21:17:37 +0200

    Add loading mask

--- src/components/nextGeneration/NextGeneration.tsx
+++ src/components/nextGeneration/NextGeneration.tsx
@@ -1,20 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import styles from './nextGeneration.module.scss';
 import { RefsContext } from '../../providers/Refs';
 
 export const NextGeneration = () => {
-const { nextGenButtonRef } = useContext(RefsContext);
+  const { nextGenButtonRef } = useContext(RefsContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const timerRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => () => clearTimeout(timerRef.current), []);
 
   const onButtonClick = () => {
     if (!nextGenButtonRef.current) return;
 
     const event = new Event("nextGeneration");
+    
+    setIsLoading(true);
 
-    nextGenButtonRef.current.dispatchEvent(event);
+    timerRef.current = setTimeout(() => {
+      setIsLoading(false);
+      nextGenButtonRef.current?.dispatchEvent(event);
+    }, 700);
   };
 
   return (
-    <button className={styles.button} onClick={onButtonClick} ref={nextGenButtonRef}>
+    <button
+      className={styles.button}
+      disabled={isLoading}
+      onClick={onButtonClick}
+      ref={nextGenButtonRef}
+    >
       next generation
     </button>
   );
